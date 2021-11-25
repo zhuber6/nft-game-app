2fcc3b4924f096b8e7267ace294ddaeb011d35b4
zjhuber <zhuber6@gmail.com>
2021-11-25 08:15:18 -0700

    Added Arena functionality against big boss

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { ethers } from 'ethers';
 import SelectCharacter from './Components/SelectCharacter';
 import { CONTRACT_ADDRESS, transformCharacterData } from './constants';
 import myEpicGame from './utils/MyEpicGame.json';
+import Arena from './Components/Arena';
 
 // Constants
 const TWITTER_HANDLE = '_buildspace';
@@ -59,6 +60,8 @@ const App = () => {
       );
     } else if (currentAccount && !characterNFT) {
       return <SelectCharacter setCharacterNFT={setCharacterNFT} />;
+    } else if (currentAccount && characterNFT) {
+      return <Arena characterNFT={characterNFT} setCharacterNFT={setCharacterNFT} />
     }
   };
 
